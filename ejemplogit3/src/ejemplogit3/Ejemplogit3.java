5b4b80a8de610ee99dcd71dc9812113b62365ed7
leviathandavid <shalashaska-1992@hotmail.com>
2017-07-13 18:24:00 -0500

    nuevo mensaje

--- ejemplogit3/src/ejemplogit3/Ejemplogit3.java
+++ ejemplogit3/src/ejemplogit3/Ejemplogit3.java
@@ -15,7 +15,8 @@ public class Ejemplogit3 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("hola");
+        
+        System.out.println("hola de nuevo");
     }
     
 }
